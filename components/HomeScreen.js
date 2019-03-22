8eda986a08cfdaa4736e194932daa481832a22f6
SamHadavi <s.hadavi@gmail.com>
2019-03-22 11:06:31 -0700

    more base

---
+++ components/HomeScreen.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class HomeScreen extends React.Component {}
---
+++ components/PianoScreen.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default class PianoScreen extends React.Component {}
