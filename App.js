caf1c363967328e63a7ff87ac81d63f00352bbeb
Kalen R Tara <32000480+kalentara@users.noreply.github.com>
2019-04-10 20:46:21 -0700

    update 4 test

--- App.js
+++ App.js
@@ -1,32 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Home from './view/Home';
-//import Piano_comp from './view/Piano_comp';
-//import List from './view/List';
-
-const RootStack = createStackNavigator(
-  {
-    Home: {
-      screen: Home,
-    },
-    Piano: {
-      screen: Home,
-    },
-    List: {
-      screen: Home,
-    }
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
-const AppContainer = createAppContainer(RootStack);
+import HomePage from './components/HomePage'
+import MapPage from './components/MapPage'
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppContainer />
+      <View style={styles.container}>
+        <MapPage/>
+      </View>
     );
   }
 }
@@ -39,10 +21,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-//COLOR SCHEME:
-//Title text: #353857
-//button background: 
-//Piano black keys: #1F1F1F 
-//Piano white keys: #F6F6F6
-//key spacing: #D9D9D9 
